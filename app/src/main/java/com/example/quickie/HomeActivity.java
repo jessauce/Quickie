a944b5e41406882cb92dd7fc1d762521dca78f5d
omero.jessica <omero.jessica@gmail.com>
2023-07-10 15:33:49 +0800

    added origin and destination items in home

--- app/src/main/java/com/example/quickie/HomeActivity.java
+++ app/src/main/java/com/example/quickie/HomeActivity.java
@@ -12,13 +12,13 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        String[] originItems = {"Naga Terminal", "San Fernando Terminal", "Carcar Terminal", "Argao Terminal"};
+        String[] originItems = {"Cebu South Bus Terminal"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, originItems);
         AutoCompleteTextView originComboBox = findViewById(R.id.originComboBox);
         originComboBox.setAdapter(adapter);
         originComboBox.setThreshold(0); // Set threshold to 0 for immediate dropdown appearance
 
-        String[] destinationItems = {"Cebu South Bus Terminal"};
+        String[] destinationItems = {"Naga Terminal", "San Fernando Terminal", "Carcar Terminal", "Argao Terminal", "Dalaguete Terminal", "Alcoy Terminal", "Boljoon Terminal", "Osblob Terminal", "Santander Terminal"};
         ArrayAdapter<String> dadapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, destinationItems);
         AutoCompleteTextView destinationComboBox = findViewById(R.id.destinationComboBox);
         destinationComboBox.setAdapter(dadapter);
