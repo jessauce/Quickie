8c04f77aa1bd820f9d8d3292889a8d2d2c2dfdfc
omero.jessica <omero.jessica@gmail.com>
2023-07-10 22:43:56 +0800

    modified sign up page and added a "please fill all fields feature" etc

--- app/src/main/java/com/example/quickie/SignUp.java
+++ app/src/main/java/com/example/quickie/SignUp.java
@@ -11,6 +11,10 @@ import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
 
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private EditText emailEditText;
+    private EditText phoneEditText;
     private EditText usernameEditText;
     private EditText passwordEditText;
     private EditText confirmPasswordEditText;
@@ -21,6 +25,10 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        firstNameEditText = findViewById(R.id.firstNameEditText);
+        lastNameEditText = findViewById(R.id.lastNameEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        phoneEditText = findViewById(R.id.phoneEditText);
         usernameEditText = findViewById(R.id.usernameEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
         confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
@@ -29,21 +37,27 @@ public class SignUp extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = usernameEditText.getText().toString();
+                String firstName = firstNameEditText.getText().toString().trim();
+                String lastName = lastNameEditText.getText().toString().trim();
+                String email = emailEditText.getText().toString().trim();
+                String phone = phoneEditText.getText().toString().trim();
+                String username = usernameEditText.getText().toString().trim();
                 String password = passwordEditText.getText().toString();
                 String confirmPassword = confirmPasswordEditText.getText().toString();
 
-                if (password.equals(confirmPassword)) {
-                    // Passwords match, show success message and navigate to the main activity
+                if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || phone.isEmpty()
+                        || username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                    Toast.makeText(SignUp.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                } else if (!password.equals(confirmPassword)) {
+                    Toast.makeText(SignUp.this, "Passwords don't match", Toast.LENGTH_SHORT).show();
+                } else {
+                    // All fields are filled and passwords match, show success message and navigate to the main activity
                     Toast.makeText(SignUp.this, "Account created", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(SignUp.this, MainActivity.class);
                     intent.putExtra("username", username);
                     intent.putExtra("password", password);
                     startActivity(intent);
                     finish(); // Optional: Finish the sign-up activity
-                } else {
-                    // Passwords don't match, show error message
-                    Toast.makeText(SignUp.this, "Passwords don't match", Toast.LENGTH_SHORT).show();
                 }
             }
         });
