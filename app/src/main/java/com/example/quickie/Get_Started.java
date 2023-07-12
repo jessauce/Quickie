43760dd3edcd9c75ff96d8dc7b5411903bee7f09
omero.jessica <omero.jessica@gmail.com>
2023-07-13 00:02:21 +0800

    updated the profile activity and added accept terms and conditions in get started

--- app/src/main/java/com/example/quickie/Get_Started.java
+++ app/src/main/java/com/example/quickie/Get_Started.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
 
 public class Get_Started extends AppCompatActivity {
 
@@ -15,10 +18,17 @@ public class Get_Started extends AppCompatActivity {
         setContentView(R.layout.activity_get_started);
 
         Button startButton = findViewById(R.id.startButton);
+        final CheckBox checkBox = findViewById(R.id.checkBox);
+
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navigateToMainActivity();
+                if (checkBox.isChecked()) {
+                    navigateToMainActivity();
+                } else {
+                    // Checkbox is not checked, display an error message or perform any other action
+                    Toast.makeText(Get_Started.this, "Please accept the terms and conditions", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/quickie/Profile.java
+++ app/src/main/java/com/example/quickie/Profile.java
@@ -1,18 +1,131 @@
 package com.example.quickie;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import java.io.File;
+import androidx.core.content.FileProvider;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import android.os.Environment;
+
+
+import java.io.IOException;
 
 public class Profile extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private ImageView profileImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        profileImageView = findViewById(R.id.profileImageView);
+
+        TextView changeImageButton = findViewById(R.id.changeImageButton);
+        changeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImagePicker();
+            }
+        });
+
+        TextView otherTextView = findViewById(R.id.otherTextView);
+        otherTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+
+        TextView aboutUsLayout = findViewById(R.id.aboutusEditText);
+        aboutUsLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToAboutUs();
+            }
+        });
+
+        TextView signOutLayout = findViewById(R.id.signoutEditText);
+        signOutLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToMainActivity();
+            }
+        });
+
     }
+
+
+    private void openImagePicker() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+    private static final int REQUEST_IMAGE_CAPTURE = 2;
+    private Uri photoUri;
+
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                return;
+            }
+            if (photoFile != null) {
+                photoUri = FileProvider.getUriForFile(this, getPackageName() + ".fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName, ".jpg", storageDir);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), photoUri);
+                profileImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                profileImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void onTicketsButtonClick(View view) {
         // Navigate to Tickets activity
         Intent intent = new Intent(Profile.this, ticket_page.class);
@@ -20,8 +133,20 @@ public class Profile extends AppCompatActivity {
     }
 
     public void onHomeButtonClick(View view) {
-        // Navigate to Profile activity
+        // Navigate to Home activity
         Intent intent = new Intent(Profile.this, HomeActivity.class);
         startActivity(intent);
     }
+    public void navigateToAboutUs() {
+        // Navigate to About Us activity
+        Intent intent = new Intent(Profile.this, AboutUs.class);
+        startActivity(intent);
+    }
+
+    public void navigateToMainActivity() {
+        // Navigate to Main activity (sign out)
+        Intent intent = new Intent(Profile.this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
