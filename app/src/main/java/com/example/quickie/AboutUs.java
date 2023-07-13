204775994a2c107a7de7b0510630c748b7751ee3
omero.jessica <omero.jessica@gmail.com>
2023-07-13 12:46:46 +0800

    updated about us page

--- app/src/main/java/com/example/quickie/AboutUs.java
+++ app/src/main/java/com/example/quickie/AboutUs.java
@@ -2,9 +2,17 @@ package com.example.quickie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutUs extends AppCompatActivity {
+
+    public void navigateToProfile(View view) {
+        Intent intent = new Intent(AboutUs.this, Profile.class);
+        startActivity(intent);
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
