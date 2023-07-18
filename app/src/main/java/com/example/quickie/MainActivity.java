3d684b7e481156229e1d73cc7d372c7e3753f62d
RomeoGabriel <romeogabrielparco@gmail.com>
2023-07-18 14:55:20 +0800

    Fixed Sign in button. Added Forgot Password Button feature. Added OTP timer simulation. Added color red.

--- app/src/main/java/com/example/quickie/MainActivity.java
+++ app/src/main/java/com/example/quickie/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.quickie;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.res.ColorStateList;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.UnderlineSpan;
@@ -20,6 +20,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,9 +28,15 @@ public class MainActivity extends AppCompatActivity {
     EditText edtEmail, edtPassword;
     ProgressBar progressBar;
     Button btnSignIn;
+    TextView txtForgotPassword;
+    TextView countdownTimer;
+    TextView otpExpiresInText; // Added reference to the "OTP expires in:" TextView
     String txtEmail, txtPassword;
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
     private FirebaseAuth mAuth;
+    private int wrongPasswordCounter = 0;
+    private CountDownTimer countDownTimer;
+    private final long countdownDuration = 60000; // Countdown duration in milliseconds (1 minute)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +56,16 @@ public class MainActivity extends AppCompatActivity {
         edtPassword = findViewById(R.id.edtSignInPassword);
         progressBar = findViewById(R.id.progressBar2);
         btnSignIn = findViewById(R.id.btnSignIn);
+        txtForgotPassword = findViewById(R.id.forgotPassword);
+        countdownTimer = findViewById(R.id.countdownTimer);
+        otpExpiresInText = findViewById(R.id.otpExpiresInText); // Initialize the "OTP expires in:" TextView
 
-        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+
+        progressBar.setVisibility(View.INVISIBLE);
+        txtForgotPassword.setVisibility(View.INVISIBLE);
+        countdownTimer.setVisibility(View.INVISIBLE);
+        otpExpiresInText.setVisibility(View.INVISIBLE); // Set the visibility to invisible initially
 
         txtSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,13 +97,25 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        txtForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Show a message to the user
+                Toast.makeText(MainActivity.this, "We have sent a 6-digit OTP to your email.", Toast.LENGTH_SHORT).show();
+                startCountdown();
+                txtForgotPassword.setVisibility(View.INVISIBLE);
+                countdownTimer.setVisibility(View.VISIBLE);
+                otpExpiresInText.setVisibility(View.VISIBLE); // Set the "OTP expires in:" TextView visible
+            }
+        });
     }
 
     private void signInUser() {
         progressBar.setVisibility(View.VISIBLE);
-        btnSignIn.setEnabled(false); // Disable the button
-        btnSignIn.setTextColor(getResources().getColor(R.color.button_disabled_color)); // Set the text color
-        btnSignIn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.button_enabled_color))); // Set the original background color
+        btnSignIn.setEnabled(false);
+        btnSignIn.setTextColor(getResources().getColor(R.color.button_disabled_color));
+        btnSignIn.setBackgroundTintList(getResources().getColorStateList(R.color.button_enabled_color));
 
         mAuth.signInWithEmailAndPassword(txtEmail, txtPassword)
                 .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
@@ -104,10 +130,65 @@ public class MainActivity extends AppCompatActivity {
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "Error - " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        String errorMessage;
+                        if (e instanceof FirebaseAuthInvalidUserException) {
+                            errorMessage = "User does not exist.";
+                        } else {
+                            errorMessage = "Error - " + e.getMessage();
+                        }
+
+                        if (wrongPasswordCounter >= 14) {
+                            errorMessage = "Too many failed attempts. Try again later.";
+                        }
+
+                        Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
                         progressBar.setVisibility(View.INVISIBLE);
-                        btnSignIn.setVisibility(View.VISIBLE);
+                        btnSignIn.setEnabled(true);
+                        btnSignIn.setTextColor(getResources().getColor(R.color.white));
+                        btnSignIn.setBackgroundTintList(getResources().getColorStateList(R.color.button_enabled_color));
+
+                        wrongPasswordCounter++;
+
+                        if (wrongPasswordCounter >= 3) {
+                            txtForgotPassword.setVisibility(View.VISIBLE);
+                        }
                     }
                 });
+    }
+
+    private void startCountdown() {
+        countDownTimer = new CountDownTimer(countdownDuration, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long minutes = millisUntilFinished / 1000 / 60;
+                long seconds = (millisUntilFinished / 1000) % 60;
+                String countdownText = String.format("%02d:%02d", minutes, seconds);
+                countdownTimer.setText(countdownText);
+
+                // Change the text color to red when 30 seconds or less are remaining
+                if (millisUntilFinished <= 30000) {
+                    countdownTimer.setTextColor(getResources().getColor(R.color.red));
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                countdownTimer.setText("00:00");
+                txtForgotPassword.setVisibility(View.VISIBLE);
+                countdownTimer.setVisibility(View.INVISIBLE);
+                otpExpiresInText.setVisibility(View.INVISIBLE); // Set the "OTP expires in:" TextView invisible
+                wrongPasswordCounter = 0; // Reset the wrong password counter
+            }
+        };
+
+        countDownTimer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
     }
 }
