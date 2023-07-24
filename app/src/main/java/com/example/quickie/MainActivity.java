fb4a8432fe6f8a4d23271a29ae30d4774e8e2ca0
RomeoGabriel <romeogabrielparco@gmail.com>
2023-07-24 23:13:54 +0800

    Edited the Send Email button

--- app/src/main/java/com/example/quickie/MainActivity.java
+++ app/src/main/java/com/example/quickie/MainActivity.java
@@ -57,6 +57,10 @@ public class MainActivity extends AppCompatActivity {
     private boolean sendViaSmsClicked = false;
     private boolean sendViaEmailClicked = false;
 
+    private String receivedOTP;
+    private long otpExpirationTime;
+    private boolean isUsingOTP = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -192,6 +196,9 @@ public class MainActivity extends AppCompatActivity {
                 sendViaSmsClicked = false;
 
                 // Add the functionality to send OTP via Email here
+                sendOtpViaEmail(otp); // Call the sendOtpViaEmail method with the user's entered email as the recipient
+
+                // Add the functionality to send OTP via Email here
                 // For now, let's just show a Toast message indicating this feature is not yet implemented
                 Toast.makeText(MainActivity.this, "Sending OTP via Email (Not implemented yet)", Toast.LENGTH_SHORT).show();
                 startCountdown();
@@ -201,8 +208,62 @@ public class MainActivity extends AppCompatActivity {
                 txtSendViaSms.setVisibility(View.INVISIBLE);
             }
         });
+
+        // Check if the user is using OTP for sign-in
+        if (isUsingOTP && receivedOTP != null && !receivedOTP.isEmpty()) {
+            // Set the email and OTP in their respective fields for convenience
+            edtEmail.setText(txtEmail);
+            edtPassword.setText(receivedOTP);
+        }
+
+
+        txtSendViaEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Set the flag to true and the other to false
+                sendViaEmailClicked = true;
+                sendViaSmsClicked = false;
+
+                // Add the functionality to send OTP via Email here
+                sendOtpViaEmail(otp); // Call the sendOtpViaEmail method with the stored recipient email
+
+                startCountdown();
+
+                // Set visibility of "Send via SMS" and "Send via Email" TextViews
+                txtSendViaEmail.setVisibility(View.VISIBLE);
+                txtSendViaSms.setVisibility(View.INVISIBLE);
+            }
+        });
+
+
     }
 
+
+    private void sendOtpViaEmail(String otp) {
+        String recipientEmail = edtEmail.getText().toString().trim(); // Get the email entered by the user
+        String senderEmail = "romeogabrielparco@gmail.com"; // Your stored recipient email
+        String subject = "OTP for Forgot Password";
+        String message = "The OTP for forgot password login access is " + otp + ".\nReminder: Never share your OTP with anyone.";
+
+        // Create an Intent to send an email
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("message/rfc822");
+
+        // Set the recipient's email address
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{recipientEmail});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{senderEmail}); // Set your email as the sender
+
+        // Verify that the device has an email client to handle the Intent
+        if (emailIntent.resolveActivity(getPackageManager()) != null) {
+            // Start the email activity
+            startActivity(emailIntent);
+        } else {
+            // If no email client is available, show a fallback message
+            Toast.makeText(MainActivity.this, "No email client found on the device. Please set up an email account or try sending via SMS.", Toast.LENGTH_LONG).show();
+        }
+    }
 
     private void startCountdown() {
         // Start the countdown timer and show the "OTP expires in" text only if either sendViaSmsClicked or sendViaEmailClicked is true
@@ -232,6 +293,12 @@ public class MainActivity extends AppCompatActivity {
                     txtSendViaSms.setVisibility(View.INVISIBLE);
                     txtSendViaEmail.setVisibility(View.INVISIBLE);
                     wrongPasswordCounter = 0; // Reset the wrong password counter
+
+
+                    // Reset the OTP-related fields
+                    receivedOTP = null;
+                    otpExpirationTime = 0;
+
                 }
             };
             countDownTimer.start();
@@ -252,43 +319,74 @@ public class MainActivity extends AppCompatActivity {
         btnSignIn.setTextColor(getResources().getColor(R.color.button_disabled_color));
         btnSignIn.setBackgroundTintList(getResources().getColorStateList(R.color.button_enabled_color));
 
-        mAuth.signInWithEmailAndPassword(txtEmail, txtPassword)
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        String errorMessage;
-                        if (e instanceof FirebaseAuthInvalidUserException) {
-                            errorMessage = "User does not exist.";
-                        } else {
-                            errorMessage = "Error - " + e.getMessage();
+        // Check if the receivedOTP is not empty and is still valid (not expired)
+        if (receivedOTP != null && !receivedOTP.isEmpty() && System.currentTimeMillis() < otpExpirationTime) {
+
+            // Check if the entered password matches the received OTP
+            if (txtPassword.equals(receivedOTP)) {
+                // Sign in the user using the OTP
+                Toast.makeText(MainActivity.this, "Temporary PIN is correct. Logging in...", Toast.LENGTH_SHORT).show();
+
+                // Clear the password field after successful login
+                edtPassword.setText("");
+
+                // Redirect the user to the HomeActivity after successful login
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+
+                // Set the flag to true to track that the user is using the OTP for sign-in
+                isUsingOTP = true;
+            } else {
+                // Show error message if the entered password doesn't match the received OTP
+                Toast.makeText(MainActivity.this, "Invalid Temporary PIN", Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
+                btnSignIn.setEnabled(true);
+                btnSignIn.setTextColor(getResources().getColor(R.color.white));
+                btnSignIn.setBackgroundTintList(getResources().getColorStateList(R.color.button_enabled_color));
+            }
+        } else {
+            // OTP is expired or not received yet, proceed with the email and password sign-in
+            // Using Firebase signInWithEmailAndPassword for regular email and password sign-in
+
+            mAuth.signInWithEmailAndPassword(txtEmail, txtPassword)
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
                         }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            String errorMessage;
+                            if (e instanceof FirebaseAuthInvalidUserException) {
+                                errorMessage = "User does not exist.";
+                            } else {
+                                errorMessage = "Error - " + e.getMessage();
+                            }
 
-                        if (wrongPasswordCounter >= 14) {
-                            errorMessage = "Too many failed attempts. Try again later.";
+                            if (wrongPasswordCounter >= 14) {
+                                errorMessage = "Too many failed attempts. Try again later.";
+                            }
+
+                            Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.INVISIBLE);
+                            btnSignIn.setEnabled(true);
+                            btnSignIn.setTextColor(getResources().getColor(R.color.white));
+                            btnSignIn.setBackgroundTintList(getResources().getColorStateList(R.color.button_enabled_color));
+
+                            wrongPasswordCounter++;
+
+                            if (wrongPasswordCounter >= 3) {
+                                txtForgotPassword.setVisibility(View.VISIBLE);
+                            }
                         }
-
-                        Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
-                        progressBar.setVisibility(View.INVISIBLE);
-                        btnSignIn.setEnabled(true);
-                        btnSignIn.setTextColor(getResources().getColor(R.color.white));
-                        btnSignIn.setBackgroundTintList(getResources().getColorStateList(R.color.button_enabled_color));
-
-                        wrongPasswordCounter++;
-
-                        if (wrongPasswordCounter >= 3) {
-                            txtForgotPassword.setVisibility(View.VISIBLE);
-                        }
-                    }
-                });
+                    });
+        }
     }
 
 
@@ -300,13 +398,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendMessageWithOTP(String phoneNumber, String otp) {
-        String message = "The OTP for forgot password login access is " + otp + ". Reminder: Never share your OTP with anyone.";
+        // Check if phoneNumber is not null
+        if (phoneNumber != null) {
+            // Save the OTP and its expiration time
+            receivedOTP = otp;
+            otpExpirationTime = System.currentTimeMillis() + countdownDuration;
 
-        try {
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNumber, null, message, null, null);
-        } catch (Exception e) {
-            Toast.makeText(MainActivity.this, "Failed to send OTP via SMS.", Toast.LENGTH_SHORT).show();
+            String message = "The OTP for forgot password login access is " + otp + ". This will serve as your temporary password. Reminder: Never share your OTP with anyone. I love you ma'am Fig <3.";
+
+            try {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "Failed to send OTP via SMS.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            // Handle the case when phoneNumber is null
+            Toast.makeText(MainActivity.this, "Phone number is not available.", Toast.LENGTH_SHORT).show();
         }
     }
 }
