411d28ef800ac7c19ca2d14292cf85ce82f64a70
klntnai <klintian.jumapit@cit.edu>
2023-07-25 00:36:00 +0800

    Gcash layout in the forest

---
+++ app/src/main/java/com/example/quickie/Gcash3.java
@@ -0,0 +1,14 @@
+package com.example.quickie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gcash3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gcash3);
+    }
+}
